841a4f4aecac05c76d5cbb0c06d5cfdb3fb641dd
Macbook <lmchvz@outlook.com>
2019-12-07 23:25:39 -0500

    updated app.js file

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,45 @@
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Loop through to append table with data
+data.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+// Get a reference to the button
+var button = d3.select("#filter-btn");
+
+// Attach event 
+button.on("click", () => {
+
+    // Select the input element
+    var inputElement = d3.select("#datetime");
+
+    // Get the value of the element
+    var inputValue = inputElement.property("value");
+ 
+    // Clear out current data in table 
+    tbody.html("");
+    
+    // Filter function 
+    function dateSelection(dateval){
+        return dateval.datetime == inputValue;
+    }
+
+    // Apply our filter
+    var tableData = data.filter(dateSelection);
+    
+    // Loop through to append table with New Data
+    tableData.forEach((newData) => {
+        var row = tbody.append("tr");
+        Object.entries(newData).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+
+});
